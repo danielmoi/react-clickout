7fcb313dd7397dc4be9cd1879bfb00e4ba123404
danielmoi <daniel@ampersandmoi.com>
2017-01-21 11:36:22 +1100

    initial setup test env

---
+++ test/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { expect } from 'chai';
+import ReactDOM from 'react-dom';
+
+describe('Hello', () => {
+  it('should render a wrapped component', () => {
+
+
+  });
+});
